e280207ce74d58163a19c8acc21860e11fddb6ce
Liam Blanton <liamblanton@gmail.com>
2023-12-14 08:47:12 -0800

    Added user input dictionary and function

--- venv/Main.py
+++ venv/Main.py
@@ -30,7 +30,58 @@ startingBoard = [[wRook(), wKnight(), wBishop(), wQueen(), wKing(), wBishop(), w
 #Create the main Board object
 mainBoard = Board(startingBoard)
 
+#Userinput Items
+uInParse = {
+    'a': 0,
+    'b': 1,
+    'c': 2,
+    'd': 3,
+    'e': 4,
+    'f': 5,
+    'g': 6,
+    'h': 7,
+    '1': 0,
+    '2': 1,
+    '3': 2,
+    '4': 3,
+    '5': 4,
+    '6': 5,
+    '7': 6,
+    '8': 7
+}  # Dictionary to parse user input
+#Function to take and convert user input (returns a move that can be used to make a legal move on the mainBoard)
+#Function contains a lof of messy conversions
+def takeUIn():
+    #Get the move
+    uIn = list(input("Enter your move: "))
+    #Loop through and parse the move, check if the move is right length, and throw error if not a valid row/col
+    for i in range(4):
+        if len(uIn) != 4:
+            break
+        try:
+            uIn[i] = uInParse[uIn[i]]
+        except KeyError:
+            uIn[i] = 9
+    #Convert to be able to check against mainBoard.moves
+    if len(uIn) == 4:
+        temp = uIn[0]
+        uIn[0] = uIn[2]
+        uIn[2] = temp
+        temp = uIn[1]
+        uIn[1] = uIn[3]
+        uIn[3] = temp
+        uIn = int(''.join(str(digit) for digit in uIn))
+    #Rerun if not in list of mainBoard.moves
+    if not mainBoard.moves.__contains__(uIn):
+        print("Enter a valid move")
+        return takeUIn()
+    return uIn
+
 #Loop
 mainBoard.PrintBoard()
 print(mainBoard.moves)
 print(len(mainBoard.moves))
+
+move = takeUIn()
+
+print(move)
